c5298d832197b1eb005013cd7ad687ef9305ebaf
Milton Medina C <miltonmce@gmail.com>
2020-08-12 21:52:06 -0700

    layout of inventory app

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -4,20 +4,31 @@
     <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>{{ this.$route.meta.title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn icon>
+          <v-icon>{{ inboxIcon }}</v-icon>
+        </v-btn>
+        <UserOptions v-on="on"></UserOptions>
+      </v-toolbar-items>
     </v-app-bar>
   </div>
 </template>
 
 <script>
 import SideBar from "@/components/sidebar/SideBar";
+import UserOptions from "@/components/navbar/UserOptions";
+import { mdiEmail } from "@mdi/js";
 
 export default {
   name: "Navbar",
   components: {
     SideBar,
+    UserOptions,
   },
   data: () => ({
     drawer: false,
+    inboxIcon: mdiEmail,
   }),
   methods: {},
 };
---
+++ frontend/src/components/navbar/UserOptions.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-menu open-on-hover bottom offset-y>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="#428bca" dark v-bind="attrs" v-on="on">
+        Login
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item v-for="(item, index) in items" :key="index">
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: "UserOptions",
+};
+</script>
